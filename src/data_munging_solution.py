a1bad1cac46fa2ec28c47481120a9310cf9ef99d
Niket Nishi <niket.nishi@mountblue.io>
2018-09-18 12:57:50 +0530

    removed one function

--- src/data_munging_solution.py
+++ src/data_munging_solution.py
@@ -1,9 +1,11 @@
 class CreateDataStructure:
-    def __init__(self, max_index, min_index):
+    def __init__(self,out_index, max_index, min_index, data_url):
         self.data_lst = []
         self.min_difference_lst = []
+        self.output_index = out_index
         self.max_index = max_index
         self.min_index = min_index
+        self.url = data_url
 
     def create_structure(self, file_url):
         with open(file_url, 'r') as file_handle:
@@ -25,17 +27,12 @@ class ComputeOutput(CreateDataStructure):
                                                float(self.data_lst[line][self.min_index])))
         return self.min_difference_lst.index(min(self.min_difference_lst))
 
-    def cal_min_temp_spread(self):
-        min_temp_lst = self.create_structure('/home/dell/PycharmProjects/data_munging_problem/data/weather.dat')
+    def compute_output(self):
+        min_diff_lst = self.create_structure(self.url)
         min_index = self.find_minimum_index()
-        return min_temp_lst[min_index + 1][0]
-
-    def min_goal_difference_team(self):
-        goal_diff_lst = self.create_structure('/home/dell/PycharmProjects/data_munging_problem/data/football.dat')
-        min_goal_index = self.find_minimum_index()
-        return goal_diff_lst[min_goal_index + 1][1]
+        return min_diff_lst[min_index + 1][self.output_index]
 
 
 if __name__ == '__main__':
-    print(ComputeOutput(1, 2).cal_min_temp_spread())
-    print(ComputeOutput(6, 8).min_goal_difference_team())
+    print(ComputeOutput(0, 1, 2, '/home/dell/PycharmProjects/data_munging_problem/data/weather.dat').compute_output())
+    print(ComputeOutput(1, 6, 8, '/home/dell/PycharmProjects/data_munging_problem/data/football.dat').compute_output())
